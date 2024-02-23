86e901ece57ae4e74d632b9b428cdfdfda033a76
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-02-23 14:38:39 +0600

    Puzzle Hunt

---
+++ Data Structure/Array/Codechef problem solved/404_Not_Found.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef vector<int> vi;
+typedef pair<int, int> p;
+ 
+#define w(t)            int t;cin>>t;while(t--)
+#define Alauddin        ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+ 
+int main()
+{
+    int x;cin >> x;
+    if(x==404) cout<<"NOT FOUND"<<endl;
+    else cout<<"FOUND"<<endl;
+    return 0;
+}
