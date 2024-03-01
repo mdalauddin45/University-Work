a222ce58313a50a41716d7662204c38472b9ea16
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-03-01 14:22:48 +0600

    bit wise operation

---
+++ Data Structure/Array/Self Study/Bit/bitwise and.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef vector<int> vi;
+typedef pair<int, int> p;
+typedef long long ll;
+typedef long long int lli;
+ 
+#define w(t)            int t;cin>>t;while(t--)
+#define Alauddin        ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+ 
+int main()
+{
+    Alauddin
+    int a=7, b=4;
+    
+    int c = a & b; //AND
+    int d = a | b; //OR
+    int e = a ^ b; //XOR
+    int f = ~b ; //NOT
+    int g = a<<b ; //left shift
+    int h = a>>b ; //Right shift
+    cout<<h<<endl;
+    return 0;
+}
